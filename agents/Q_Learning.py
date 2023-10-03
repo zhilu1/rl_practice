230275870e86280ed0ecaeebc6d9ecc8e625c030
zhilu1 <zilu.dong@outlook.com>
2023-10-03 19:28:03 +0800

    did some td learning

--- agents/Q_Learning.py
+++ agents/Q_Learning.py
@@ -1,15 +1,14 @@
 from collections import defaultdict
 import numpy as np
 
-class NstepSarsaAgent:
+class QLearningAgent:
     def __init__(self,
                 action_space_n,
-                step_num = 1, # default is standard sarsa 
                 initial_q = None,
                 epsilon = 0.1,
-                learning_rate = 0.01
+                learning_rate = 0.01,
+                behavior_policy = None
                 ) -> None:
-        self.step_num = step_num # TODO 这个数值到底影响什么?
         self.action_space_n = action_space_n
         self.epsilon = epsilon
         # self.policy = defaultdict(lambda: np.zeros(self.action_space_n))
@@ -23,9 +22,11 @@ class NstepSarsaAgent:
                 self.policy_improvement(state)
         else:
             self.q = defaultdict(lambda: np.zeros(action_space_n))
+        if behavior_policy:
+            self.behavior_policy = behavior_policy
+        else:
+            self.behavior_policy = defaultdict(lambda: np.ones(self.action_space_n) * (1/self.action_space_n))
             
-    def update_q(self, state, action, reward, next_state, next_action, discounted_factor):
-        self.q[state][action] = self.q[state][action] - self.learning_rate * ( self.q[state][action] - (reward + discounted_factor * self.q[next_state][next_action]))
 
     def td_learn(self, state, action,  td_target):
         self.q[state][action] = self.q[state][action] - self.learning_rate * ( self.q[state][action] - td_target)
@@ -41,3 +42,6 @@ class NstepSarsaAgent:
         if get_optimal:
             return np.argmax(self.policy[state])
         return np.random.choice(len(self.policy[state]),1,p=self.policy[state])[0] # random choose an action based on policy
+        
+    def get_behavior_acion(self, state):
+        return np.random.choice(len(self.behavior_policy[state]),1,p=self.behavior_policy[state])[0] # random choose an action based on policy
--- perform_TD_learning.py
+++ perform_TD_learning.py
@@ -7,7 +7,6 @@ from collections import defaultdict
 
 # from rl_envs.episodic_grid_world_env import EpisodicGridWorldEnv as GridWorldEnv
 from rl_envs.grid_world_env import GridWorldEnv
-from agents.nstep_Sarsa import NstepSarsaAgent as AgentAlgo
 
 
 def print_actions(agent, env, get_optimal = False):
@@ -21,36 +20,92 @@ def print_actions(agent, env, get_optimal = False):
 
 
 
-env = GridWorldEnv(5, 4, forbidden_grids=[(2,1), (1,3)], target_grids=[(2,3)], forbidden_reward=-1, hit_wall_reward=-10)
+"""
+start at somewhere and find optimal path using n-step Sarsa
+"""
+# from agents.nstep_Sarsa import NstepSarsaAgent as AgentAlgo
+
+# env = GridWorldEnv(3, 4, forbidden_grids=[(2,1), (1,3)], target_grids=[(2,3)], forbidden_reward=-1, hit_wall_reward=-10)
+# agent = AgentAlgo(step_num=5, action_space_n=env.possible_actions)
+
+# for _ in range(5000): # find target 500 times
+#     state = (2,0) # initial_state
+#     action = agent.get_action(state)
+#     while state not in env.target_grids:
+#         rewards_ls = []
+#         next_state = state
+#         next_action = action
+#         for _ in range(agent.step_num):
+#             next_state, reward = env.step(next_state, next_action)
+#             next_action = agent.get_action(next_state)
+#             rewards_ls.append(reward)
+#             if next_state in env.target_grids:
+#                 break
+        
+#         td_target = 0
+#         for reward in reversed(rewards_ls):
+#             td_target *= env.discounted_factor
+#             td_target += reward
+        
+#         td_target += agent.q[next_state][next_action]
+#         # agent.update_q(state, action, reward, next_state, next_action, env.discounted_factor)
+#         agent.td_learn(state, action, td_target)
+#         agent.policy_improvement(state)
+#         state = next_state
+#         action = next_action
+
+"""
+perform online Q-learning
+"""
+from agents.Q_Learning import QLearningAgent as AgentAlgo
 
 
-agent = AgentAlgo(step_num=1, action_space_n=env.possible_actions)
+# env = GridWorldEnv(3, 4, forbidden_grids=[(2,1), (1,3)], target_grids=[(2,3)], forbidden_reward=-1, hit_wall_reward=-10)
+# agent = AgentAlgo(action_space_n=env.possible_actions)
 
-for _ in range(500): # find target 500 times
-    state = (2,0) # initial_state
-    action = agent.get_action(state)
+
+
+# for _ in range(500): # find target 500 times
+#     state = (2,0) # initial_state
+#     action = agent.get_action(state)
+#     while state not in env.target_grids:
+#         next_state, reward = env.step(state, action)
+#         td_target = reward + env.discounted_factor * max(agent.q[next_state])
+#         # td_target += agent.q[next_state][next_action]
+#         # agent.update_q(state, action, reward, next_state, next_action, env.discounted_factor)
+#         agent.td_learn(state, action, td_target)
+#         agent.policy_improvement(state)
+#         state = next_state
+#         action = agent.get_action(state)
+
+"""
+perform offline Q-learning, but calculating all state's optimal paths
+"""
+from agents.Q_Learning import QLearningAgent as AgentAlgo
+
+
+env = GridWorldEnv(3, 4, forbidden_grids=[(2,1), (1,3)], target_grids=[(2,3)], forbidden_reward=-1, hit_wall_reward=-10)
+agent = AgentAlgo(action_space_n=env.possible_actions, epsilon=0)
+
+state = (0,0)
+trajectorys = []
+episode_num = 500
+# collecting experiences
+for _ in range(episode_num):
+    trajectory = []
     while state not in env.target_grids:
-        rewards_ls = []
-        next_state = state
-        next_action = action
-        for _ in range(agent.step_num):
-            next_state, reward = env.step(next_state, next_action)
-            next_action = agent.get_action(next_state)
-            rewards_ls.append(reward)
-            if next_state in env.target_grids:
-                break
-        
-        td_target = 0
-        for reward in reversed(rewards_ls):
-            td_target += reward
-            td_target *= env.discounted_factor
-        
-        td_target += agent.q[next_state][next_action]
-        # agent.update_q(state, action, reward, next_state, next_action, env.discounted_factor)
+        action = agent.get_behavior_acion(state)
+        next_state, reward = env.step(state, action)
+        trajectory.append((state, action, reward , next_state))
+        state = next_state
+    trajectorys.append(trajectory)
+
+    for state, action, reward, next_state in trajectory:
+        td_target = reward + env.discounted_factor * max(agent.q[next_state])
         agent.td_learn(state, action, td_target)
         agent.policy_improvement(state)
-        state = next_state
-        action = next_action
+
+
 
 
 """
