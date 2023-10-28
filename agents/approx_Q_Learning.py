6d2455d6b106d4b9f1aad03460016e1cec3cc7ce
zhilu1 <1053455285@qq.com>
2023-10-28 17:27:22 +0800

    approximate Qlearn, fix some bug but not work well at complex

--- agents/approx_Q_Learning.py
+++ agents/approx_Q_Learning.py
@@ -1,12 +1,22 @@
+"""
+用线性基函数模型来代替 tabular Qlearning 中的 Q-table 来拟合估计 action value
+
+其中基函数 (basis function) 使用 the Fourier function of order 5 (参考 "Mathematical Foundations of Reinforcement Learning" 书上 P.176 和 P.188)
+
+"""
+
 from collections import defaultdict
 import numpy as np
 import torch
 from rl_envs.new_gym_grid_world_env import GridWorldEnv
-
-
+# import math
+from math import cos, pi
+from torch.utils.tensorboard import SummaryWriter # type: ignore
+import itertools
+from random import shuffle
 class ApproxQLearningAgent:
     def __init__(
-        self, action_space_n, epsilon=0.1, learning_rate=1e-5, discounted_factor=0.9
+        self, action_space_n, epsilon=0.1, learning_rate=1e-4, discounted_factor=0.9
     ) -> None:
         self.action_space_n = action_space_n
         self.epsilon = epsilon
@@ -16,50 +26,51 @@ class ApproxQLearningAgent:
             lambda: np.ones(self.action_space_n) * (1 / self.action_space_n)
         )
         self.learning_rate = learning_rate  # could depend on t, but never mind
-        self.parameters = [1 for _ in range(17)]
+        self.parameters = np.array([1. for _ in range(125)]) # 2^3, 3^3, ...
+        self.phi_ls = np.array(list(itertools.product(range(5), repeat=3))) 
 
-    def phi_sa(self, state, action, i):
-        x, y, z = *state, action
-        t = [
-            1,
-            x,
-            y,
-            z,
-            x * y,
-            y * z,
-            x * z,
-            x * x,
-            y * y,
-            z * z,
-            x * x * z,
-            y * y * z,
-            x * x * y,
-            y * y * x,
-            z * z * x,
-            z * z * y,
-            x * y * z,
-        ]
-        # t = {0: 1, 1:x, 2: y, 3:z, 4: x*y, 5: y*z, 6: x*z,7: x*x,}  # φ(s) = [1, x, y, x^2, y^2, xy]T ∈ R6.
-        # t = {0: 1, 1:state[0], 2: state[1], 3:state[0]**2, 4: state[1]**2,5: state[0]*state[1]}  # φ(s) = [1, x, y, x^2, y^2, xy]T ∈ R6.
-        # t = {0: 1, 1:cos(state[0] * pi), 2: cos(state[1] * pi), 3:cos((state[1]+state[0]) * pi)}  # φ(s) = [1, x, y, x^2, y^2, xy]T ∈ R6.
-        return t[i]
+    # def phi_sa(self, state, action, i):
+    #     x, y, z = *state, action
+    #     # the linear feature vector is selected as the Fourier function of order 5 (参考书上 P.176 和 P.188)
+    #     c_1, c_2, c_3 = self.phi_ls[i]
+    #     phi = cos(c_1*x + c_2*y + c_3*z)
+    #     # φ(s, a) = [1, cosx, cosy, cosz, cos(x+y), cos(x+z), cos(y+z), cos(y+z), cos(x+y+z), cos(2x),... ] * parameters
+    #     return phi
 
     def estimate_q(self, state, action):
-        q = 0
-        for i, param in enumerate(self.parameters):
-            q += self.phi_sa(state, action, i) * param
+        x, y, z = *state, action
+        phi_mat = np.cos(self.phi_ls @ np.array([x,y,z]))
+        q = phi_mat @ self.parameters
+        # for i, param in enumerate(self.parameters):
+        #     q += self.phi_sa(state, action, i) * param
         return q
 
     def update_parameters(self, TD_error, state, action, next_state, best_action):
-        # 可以在 update parameters 时顺便算出所有 state, action 的 phisa 并存储
-        for i, param in enumerate(self.parameters):
-            # gradient = self.phi_sa(
-            #     next_state, best_action, i
-            # ) * self.discounted_factor - self.phi_sa(state, action, i)
-            gradient = self.phi_sa(state, action, i)
-            self.parameters[i] += self.learning_rate * (2 * TD_error * gradient)
+        """
+        随机梯度下降法.
+        """
+
+        x, y, z = *state, action
+        phi_mat = np.cos(self.phi_ls @ np.array([x,y,z]))
+        x, y, z = *next_state, best_action
+        next_phi_mat = np.cos(self.phi_ls @ np.array([x,y,z]))
+        gradient = next_phi_mat* self.discounted_factor - phi_mat
+
+        # x, y, z = *state, action
+        # phi_mat = np.cos(self.phi_ls @ np.array([x,y,z]))
+        # gradient = phi_mat
+        self.parameters -= self.learning_rate * (2 * TD_error * gradient)
+        # for i, param in enumerate(self.parameters):
+        #     # gradient = self.phi_sa(
+        #     #     next_state, best_action, i
+        #     # ) * self.discounted_factor - self.phi_sa(state, action, i)
+        #     gradient = self.phi_sa(state, action, i)
+        #     self.parameters[i] += self.learning_rate * (2 * TD_error * gradient)
 
     def policy_improvement(self, state):
+        """
+        利用函数估计 Q 值, 从而建立 epsilon-greedy policy
+        """
         best_action = 0
         best_q_value = -float("inf")
         for action in range(self.action_space_n):
@@ -72,7 +83,7 @@ class ApproxQLearningAgent:
             1 - ((self.action_space_n - 1) / self.action_space_n) * self.epsilon
         )
 
-    def get_action(self, state, optimal=False) -> int:
+    def get_action(self, state, optimal=False):
         state = tuple(state)
         if optimal:
             return np.argmax(self.policy[state])
@@ -81,8 +92,12 @@ class ApproxQLearningAgent:
         ]  # random choose an action based on policy
 
     def RUN(self, env: GridWorldEnv, num_episodes=1000, episode_len=100000):
+        writer = SummaryWriter()
         next_state, _ = env.reset()
         episode_recorder = []
+        """
+        collect replay experience
+        """
         for _ in range(episode_len):
             state = next_state
             action = env.action_space.sample()
@@ -94,21 +109,23 @@ class ApproxQLearningAgent:
                     reward,
                     tuple(next_state),
                 )
-                # (state[0] / float(env.height), state[1] / float(env.width)),
-                # action / env.action_n,
-                # reward,
-                # (
-                #     next_state[0] / float(env.height),
-                #     next_state[1] / float(env.width),
-                # ),
             )
+        """
+        # input normalization                 
+        (state[0] / float(env.height), state[1] / float(env.width)),
+        action / env.action_n,
+        reward,
+        (
+            next_state[0] / float(env.height),
+            next_state[1] / float(env.width),
+        ),"""
 
         for i_episode in range(num_episodes):
+            shuffle(episode_recorder)
             for i_step, (state, action, reward, next_state) in enumerate(
                 episode_recorder
             ):
                 q_value = self.estimate_q(state, action)
-
                 next_q = -float("inf")
                 best_action = 0
                 for a in range(env.action_n):
@@ -122,6 +139,16 @@ class ApproxQLearningAgent:
                 self.update_parameters(
                     TD_error, state, action, next_state, best_action
                 )  # loss is MSE(TD_target, q_value)
+                # writer.add_scalar(
+                # "TD error", TD_error, i_episode*episode_len + i_step
+                # )
+            if i_episode % 10 == 0:
+                print("\rEpisode {}/{} ( TD_error: {})".format(
+                    i_episode, num_episodes, TD_error))
+
+        """
+        在结束时更新 policy, 供 get_action 使用
+        """
         for y in range(env.height):
             for x in range(env.width):
                 self.policy_improvement((y, x))
